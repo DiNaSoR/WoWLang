1d2a59b30f1e7428801166cfd25e3071c83814d9
Platine-WOW <platine.wow@gmail.com>
2024-07-24 20:31:12 +0200

    Missing files in translation folder

---
+++ WoWAR/Translations/TlumaczOnline_AR.lua
@@ -0,0 +1,24 @@
+﻿-- Arabic database of translated quest texts
+-- for the addon WoWAR-Quests
+
+TO_lang = "AR";
+TO_date = "2023-05-26";
+
+-- ÇEVRİMİÇİ çeviri tablosu
+QTR_Tlumacz_Online = {
+};
+
+function pairsByKeys (t)
+   local a = {}
+   for n in pairs(t) do table.insert(a, n) end
+   table.sort(a, function(a, b) return a > b end)
+   local i = 0      -- iterator variable
+   local iter = function ()   -- iterator function
+      i = i + 1
+      if a[i] == nil then return nil
+         else return a[i], t[a[i]]
+      end
+   end
+   return iter
+end
+
