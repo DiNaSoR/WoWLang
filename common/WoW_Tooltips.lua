07f2d5eb846c1533f7104f8594300d319040367b
Hakan YILMAZ <159566338+hknylmz-wow@users.noreply.github.com>
2025-03-28 11:54:04 +0300

    Fixed wrong font type.

--- common/WoW_Tooltips.lua
+++ common/WoW_Tooltips.lua
@@ -2165,7 +2165,7 @@ function ST_GroupFinder()
       
                      if element.SetFont then
                            -- Use select(2,...) which is safer if GetFont returns nil
-                           element:SetFont(WOWTR_Font1, select(2, element:GetFont()))
+                           element:SetFont(WOWTR_Font2, select(2, element:GetFont()))
                      end
                   -- else -- No translation found
                      -- Ensure original font remains if needed (usually not necessary unless something else modified it)
